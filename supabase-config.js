9349f558138bf7526a79c0ba29d561f72f0d4741
khodadadi2100-dot <khodadadi2100@gmail.com>
2026-08-27 20:42:54 +0330

    Load Dastchin UI and checkout fixes

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
 window.DASTCHIN_SUPABASE_URL='https://ydfjpwofursqdxtjnvdw.supabase.co';
 window.DASTCHIN_SUPABASE_PUBLISHABLE_KEY='sb_publishable_hkQHmGJCe3Mm96JvIn6QEg_N5dqCMhV';
+(function(){var s=document.createElement('script');s.src='dastchin-fix.js?v=2';s.defer=true;document.head.appendChild(s);})();
