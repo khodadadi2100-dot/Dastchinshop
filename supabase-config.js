22252c3c8b330e6ae0c74722e0bd3bbfd2609159
khodadadi2100-dot <khodadadi2100@gmail.com>
2026-08-28 12:01:20 +0330

    remove legacy UI override script

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,2 @@
 window.DASTCHIN_SUPABASE_URL='https://ydfjpwofursqdxtjnvdw.supabase.co';
 window.DASTCHIN_SUPABASE_PUBLISHABLE_KEY='sb_publishable_hkQHmGJCe3Mm96JvIn6QEg_N5dqCMhV';
-(function(){var s=document.createElement('script');s.src='dastchin-fix.js?v=3';s.defer=true;document.head.appendChild(s);})();
